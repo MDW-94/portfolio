5214549696b798ca6b80d6092eae9d402ce7e000
Matthew David Wasylko <matthew.wasylko@gmail.com>
2023-10-28 20:25:24 +0100

    photograph added to portfolio, need to plan a layout for about me page - removed Spline features - thought they would be overkill for a flick-through app

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 
-import Spline from '@splinetool/react-spline'
+import me1 from '../Images/photos_me/me1.png'
+// same for 2, 3, 4 etc - make carousel and some words!
 
 import styled from "styled-components";
 
@@ -12,59 +13,21 @@ align-items: center;
 justify-content: center;
 `
 
-const SplineScene = styled.div`
-width: 60%;
-`
-
-const Button = styled.button`
-color: black;
-font-size: 1em;
-font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-padding: 1em;
-background-color: white;
-
+const ImageStyle = styled.img`
+width: 30vw;
+border-radius: 2em;
 `
 
 
 const About = () => {
 
-    // const cube = useRef();
-    // const sphere = useRef();
-
-   
-
-    // function onLoad(spline) {
-    //     const obj = spline.findObjectById('e3bbbd58-74c6-4a77-a642-575ce8c300df');
-    //     const obj2 = spline.findObjectById('69dcbcdb-67e4-492b-b3b2-17ca574a84cd')
-    //     cube.current = obj
-    //     sphere.current = obj2
-    // }
-
-    // function moveObj() {
-    //     console.log(cube.current);
-
-    //     cube.current.position.x += 1000;
-    //     // cube.current.position.y += 10;
-    // };
-
-    // function moveObj2() {
-    //     console.log(sphere.current);
-    //     sphere.current.position.x += 100;
-    // };
 
 
     return ( 
         <AboutStyle>
             <h1>I am the About Page - Hello!</h1>
-            {/* <SplineScene>
-            {onLoad ? <Spline
-            scene="https://prod.spline.design/QhRn0ppxQkt44IOm/scene.splinecode"
-            onLoad={onLoad}
-            />: <h2>Loading...</h2>}
-            </SplineScene>
-            <br></br>
-            <Button type="button" onClick={moveObj}>Move Object</Button>
-            <Button type="button" onClick={moveObj2}>Move Object 2</Button> */}
+            <ImageStyle src={me1} alt="A profile photo of me on holiday"></ImageStyle>
+
         </AboutStyle>
      );
 }
